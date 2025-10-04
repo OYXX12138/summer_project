78e0614464e357cfe917c3f167e2f029928bd2bf
summer_project <841681730@qq.com>
2025-10-04 14:55:08 +0300

    first_commit

---
+++ TEST.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+template<typename T>
+void sort(T a)
+{
+	cout << "请输入要进行的排序："
+		<< "1.升序"
+		<< "2.倒序" << endl;
+
+	int select;
+	cin >> select;
+
+	
+
+}
+
+void test01()
+{
+	char arr[] = "acfdeb";
+
+}
+
+int main()
+{
+
+	return 0;
+	system("pause");
+	system("cls");
+}
