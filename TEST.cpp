cb56e2dc5457cdf7ad9ab78aa468d9756bd76084
summer_project <841681730@qq.com>
2025-10-04 15:22:26 +0300

    Initial commit

--- TEST.cpp
+++ TEST.cpp
@@ -1,31 +1,54 @@
-﻿#include<iostream>
-#include<string>
-using namespace std;
+#include <QApplication>
+#include "mainwindow.h"
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
+#include <QTranslator>
 
-template<typename T>
-void sort(T a)
+int main(int argc, char *argv[])
 {
-	cout << "请输入要进行的排序："
-		<< "1.升序"
-		<< "2.倒序" << endl;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 
-	int select;
-	cin >> select;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-	
+    QApplication app(argc, argv);
 
+    // 加载翻译文件
+    QTranslator translator;
+    translator.load("translations/Language_CN.qm"); // 确保翻译文件路径正确
+    QApplication::installTranslator(&translator);
+
+    QFile file(":/res/qss/style.qss");
+    file.open(QFile::ReadOnly);
+    QTextStream filetext(&file);
+    QString stylesheet = filetext.readAll();
+    app.setStyleSheet(stylesheet);
+    file.close();
+
+    /*QStringList drivers = QSqlDatabase::drivers();
+        qDebug() << "Available drivers:" << drivers;
+
+   // 添加插件路径
+        QApplication::addLibraryPath("D:/Postgraduate/Qt/5.14.1/msvc2017_64/plugins");
+
+        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName("localhost");
+        db.setDatabaseName("mysql");
+        db.setUserName("root");
+        db.setPassword("123456");
+
+        if (!db.open()) {
+            qDebug() << "无法连接到数据库: " << db.lastError().text();
+        } else {
+            qDebug() << "成功连接到数据库";
+            db.close(); // 确保连接正常后关闭数据库
+            QSqlDatabase::removeDatabase("qt_sql_default_connection");
+        }*/
+    MainWindow mainWindow;
+    mainWindow.show();
+    return app.exec();
 }
 
-void test01()
-{
-	char arr[] = "acfdeb";
-
-}
-
-int main()
-{
-
-	return 0;
-	system("pause");
-	system("cls");
-}
